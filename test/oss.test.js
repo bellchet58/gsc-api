7fe69c99d7924dbc01518465b3520296929dbf6a
liubj <liubj@belewtech.com>
2020-04-21 18:01:37 +0800

    添加到oss上传为止的功能

--- test/oss.test.js
+++ test/oss.test.js
@@ -1,9 +1,14 @@
 const init = require('../lib/init')
-const { oss } = init({ token: '115377f2ed6d6d02935fd335216e036d'})
+const { oss } = init({ token: '115377f2ed6d6d02935fd335216e036d', timeout: 99999999999999999})
+const { v4 } = require('uuid')
 
 describe("oss related api", () => {
   test("get sign", async () => {
     const result = await oss.getSign({})
-    expect(result.dir).toEqual('ipa');
+    const uploadResult = await oss.doUpload(result)
+    // expect(!!uploadResult).toBe(true)
   });
+  test("get oss upload key", () => {
+    expect(v4().replace(/\-/g, '').length).toBe(32)
+  })
 });
