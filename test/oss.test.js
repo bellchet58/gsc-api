f53a27d4f14427dfd372239974de68b9f9fc8b2b
liubj <liubj@belewtech.com>
2020-04-22 11:19:41 +0800

    经测可用测试功能

--- test/oss.test.js
+++ test/oss.test.js
@@ -1,14 +1,17 @@
 const init = require('../lib/init')
-const { oss } = init({ token: '115377f2ed6d6d02935fd335216e036d', timeout: 99999999999999999})
+const { oss, upload } = init({ token: '这是token'})
 const { v4 } = require('uuid')
 
 describe("oss related api", () => {
   test("get sign", async () => {
     const result = await oss.getSign({})
-    const uploadResult = await oss.doUpload(result)
-    // expect(!!uploadResult).toBe(true)
+    expect(result.dir).toEqual('ipa');
   });
   test("get oss upload key", () => {
     expect(v4().replace(/\-/g, '').length).toBe(32)
   })
+  test("upload", async () => {
+    const result = await upload()
+    expect(result).toEqual('https://www.gscapp.cn/app/85lT')
+  }, 20000)
 });
